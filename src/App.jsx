6e6001086d7fac01eed1c3d0d24535f9e9bdec77
Philipp Isaak <phi.isaak@gmail.com>
2024-11-03 21:13:25 +0100

    Link component and jobs page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from "react-router-dom";
 import HomePage from "./pages/HomePage.jsx";
+import JobsPage from "./pages/JobsPage.jsx";
 import MainLayout from "./layouts/MainLayout.jsx";
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<MainLayout />} >$
             <Route index element={ <HomePage />} />
+            <Route path="/jobs" element={ <JobsPage />} />
         </Route>
     )
 );
