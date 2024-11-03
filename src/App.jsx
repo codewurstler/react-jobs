8314a03ddb0e33bd30d1397d2953619fc78ac4e5
Philipp Isaak <phi.isaak@gmail.com>
2024-11-03 21:31:06 +0100

    navigation mit navlink

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider} fr
 import HomePage from "./pages/HomePage.jsx";
 import JobsPage from "./pages/JobsPage.jsx";
 import MainLayout from "./layouts/MainLayout.jsx";
+import NotFoundPage from "./pages/NotFoundPage.jsx";
 
 const router = createBrowserRouter(
     createRoutesFromElements(
-        <Route path="/" element={<MainLayout />} >$
+        <Route path="/" element={<MainLayout />} >
             <Route index element={ <HomePage />} />
             <Route path="/jobs" element={ <JobsPage />} />
+            <Route path="*" element={ <NotFoundPage /> } />
         </Route>
     )
 );
