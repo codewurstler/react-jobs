7f55279245e88f03cf87ef98c1ede4c5759e8bee
Philipp Isaak <phi.isaak@gmail.com>
2024-11-04 20:49:24 +0100

    job page fetching data with use effect

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import HomePage from "./pages/HomePage.jsx";
 import JobsPage from "./pages/JobsPage.jsx";
 import MainLayout from "./layouts/MainLayout.jsx";
 import NotFoundPage from "./pages/NotFoundPage.jsx";
+import JobPage from "./pages/JobPage.jsx";
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<MainLayout />} >
             <Route index element={ <HomePage />} />
             <Route path="/jobs" element={ <JobsPage />} />
+            <Route path="/jobs/:id" element={ <JobPage /> } />
             <Route path="*" element={ <NotFoundPage /> } />
         </Route>
     )
